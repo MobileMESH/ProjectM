ad9dcc5675cc2f0b2c090600e1b0bf57e541b3aa
Roope Mantere <roope.mantere@tuni.fi>
2023-02-15 18:49:13 +0200

    Add class constructor and basic functionality

--- ProjectM/app/src/main/java/fi/mobilemesh/projectm/MainActivity.kt
+++ ProjectM/app/src/main/java/fi/mobilemesh/projectm/MainActivity.kt
@@ -2,10 +2,13 @@ package fi.mobilemesh.projectm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fi.mobilemesh.projectm.connectionManager.ConnectionManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val connectionManager = ConnectionManager(this)
     }
 }
