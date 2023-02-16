4d37ed24552169070a19b23a32604d2250b7a668
Roope Mantere <roope.mantere@tuni.fi>
2023-02-16 10:35:23 +0200

    add permissions to androidManifest, add functions for checking permissions

--- ProjectM/app/src/main/java/fi/mobilemesh/projectm/MainActivity.kt
+++ ProjectM/app/src/main/java/fi/mobilemesh/projectm/MainActivity.kt
@@ -1,14 +1,54 @@
 package fi.mobilemesh.projectm
 
+import android.Manifest.permission.*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.wifi.p2p.WifiP2pManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import fi.mobilemesh.projectm.connectionManager.ConnectionManager
+import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE = 223312
+    }
+
+    val permissions = arrayOf(
+        "android.permission.ACCESS_WIFI_STATE",
+        "android.permission.CHANGE_WIFI_STATE",
+        "android.permission.ACCESS_FINE_LOCATION",
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.NEARBY_WIFI_DEVICES"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val connectionManager = ConnectionManager(this)
+    }
+
+    fun hasPermissions(permissions: Array<String>) {
+        for (permission in permissions)
+        {
+            if (ContextCompat.checkSelfPermission(this, permission) != PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this, arrayOf(permission), REQUEST_CODE)
+            }
+        }
+        return
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE) {
+
+        }
     }
 }
