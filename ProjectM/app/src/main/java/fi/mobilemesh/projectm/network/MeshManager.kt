882917fce37dc3db6e54cc687be50265f794e098
Roope Mantere <roope.mantere@tuni.fi>
2023-03-21 22:45:19 +0200

    add fun handleDiscoveredDevice

--- ProjectM/app/src/main/java/fi/mobilemesh/projectm/network/MeshManager.kt
+++ ProjectM/app/src/main/java/fi/mobilemesh/projectm/network/MeshManager.kt
@@ -1,4 +1,14 @@
 package fi.mobilemesh.projectm.network
 
-class MeshManager {
+import android.net.wifi.p2p.WifiP2pDevice
+
+class MeshManager(private val broadcastManager: BroadcastManager){
+    private val discoveredDevices: MutableMap<String, Device> = mutableMapOf()
+
+
+    fun handleDiscoveredDevice(device: WifiP2pDevice){
+
+    }
+    
+
 }
