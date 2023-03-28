c3e0b106a96d97b6f0cdf15b7f876de3c56348b0
Roope Mantere <roope.mantere@tuni.fi>
2023-03-28 21:42:34 +0300

    Add missing getters and refactor codebase

--- ProjectM/app/src/main/java/fi/mobilemesh/projectm/network/Device.kt
+++ ProjectM/app/src/main/java/fi/mobilemesh/projectm/network/Device.kt
@@ -2,57 +2,41 @@ package fi.mobilemesh.projectm.network
 
 import android.content.Context
 import android.net.wifi.p2p.WifiP2pDevice
+import android.location.Location
+import android.location.LocationManager
 
 open class Device (device: WifiP2pDevice) :java.io.Serializable{
-    private val macAdd = device.deviceAddress
-    private val demoAddess = "AB:CD"
-    private val name = device.deviceName
-    val availableDevices = listOf<Device>()
 
-    fun returnName(): String{
+    private val macAdd = device.deviceAddress
+    private val name = device.deviceName
+    private lateinit var location: Location
+    private var has_notifications = false
+    private var share_location = false
+    private var availableDevices = mutableListOf<Device>()
+
+    fun getName(): String{
         return name
     }
-    fun returnAddress(): String{
+
+    fun getAddress(): String{
         return macAdd
     }
-    fun returnDemoAddress(): String{
-        return demoAddess
-    }
-}
 
-
-class MyPreferences(context: Context, device: Device){
-
-    private val sharedPreferences = context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-    private lateinit var devices: Device
-
-    init {
-        devices = device
-        setDeviceAddress(device.returnAddress())
-        setDeviceName(device.returnName())
+    fun getLocation(): Location{
+        return location
     }
 
-
-
-    fun getDeviceName(): String? {
-        return sharedPreferences.getString("deviceName", devices.returnName())
-    }
-    fun getDeviceAddress(): String? {
-        return sharedPreferences.getString("deviceAddress", devices.returnAddress())
+    fun getNotification(): Boolean{
+        return has_notifications
     }
 
-    private fun setDeviceName(value: String) {
-        sharedPreferences.edit().putString("deviceName", devices.returnName()).apply()
+    fun getHasSharedLocation(): Boolean {
+        return share_location
     }
 
-    private fun setDeviceAddress(value: String) {
-        sharedPreferences.edit().putString("deviceAddress", devices.returnAddress()).apply()
+    fun getAvailableDevices(): List<Device> {
+        return availableDevices
     }
-    private fun setDemoDeviceAddress(value: String) {
-        sharedPreferences.edit().putString("demoDeviceAddress", devices.returnDemoAddress()).apply()
-    }
-    fun getDemoDeviceAddress(): String? {
-        return sharedPreferences.getString("demoDeviceAddress", devices.returnDemoAddress())
-    }
+
 }
 
