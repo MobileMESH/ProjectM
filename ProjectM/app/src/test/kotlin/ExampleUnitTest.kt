fc9c12b83b8d28fa9f5fd76100bee483bf5b259c
Roope Mantere <roope.mantere@tuni.fi>
2023-04-25 16:33:44 +0300

    Created Example Unit Test

---
+++ ProjectM/app/src/test/kotlin/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UnitTests {
+    @Test
+    fun addition_isCorrect() {
+        val sum = 2 + 2
+        assertEquals(4, sum)
+    }
+
+    @Test
+    fun subtraction_isCorrect() {
+        val difference = 4 - 2
+        assertEquals(2, difference)
+    }
+}
